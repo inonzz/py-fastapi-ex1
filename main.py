dfce8c8f1d4c9879a0d51a5c091c2f58748f458e
orenk80 <orenk80@gmail.com>
2022-08-10 23:04:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-from fastapi import FastAPI
-from fastapi_utils.inferring_router import InferringRouter
-import consts
-from controller.InstallScriptController import InstallScriptController
+import uvicorn
 
-# def print_hi(name):
-#     # Use a breakpoint in the code line below to debug your script.
-#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-#
-#
-# # Press the green button in the gutter to run the script.
+from fastapi import FastAPI
+
+app = FastAPI()
+from fastapi import  Query, Path
+
+
+from data.InstallScriptInputData import ScriptInputData
+
+from fasadImpl.InstallScriptFasadeImpl import InstallScriptFasadeImpl
+
+uri = "/RestService/InstallScript"
+install_script_fasade = InstallScriptFasadeImpl()
+
+
+@app.get(uri)
+def get_script_page():
+    return {"Data": "get_script_page"}
+
+
+@app.get(uri + "/{itemId}")
+def get_by_id(item_id: int = Path(None, description="The Id we would like to have", gt=0, lt=2)):
+    return {"Data": "get_by_id"}
+
+
+@app.get(uri + "/getByName")
+def get_by_name( name: str = Query(None, title="Name", description="The Id we would like to have")):
+    return {"Data": "get_by_name"}
+
+
+@app.post(uri + "/generateScript")
+def get_script_page( script_data: ScriptInputData):
+    return install_script_fasade.generate_install_scripts_input_page(script_data)
+
 if __name__ == '__main__':
-    url = ""
-    app = consts.app
-    c = InstallScriptController(url)
+    uvicorn.run(
+        app,
+        host="localhost",
+        port=8088,
+        timeout_keep_alive=3600
+    )
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
